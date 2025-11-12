6f42b459b3a6de9626d9e5df0eded3afadc08b81
Suraj7149 <santoshsuraj7149@gmail.com>
2025-11-12 20:44:40 +0530

    demo work completed - work section naming update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import Hero from './Components/Hero.jsx'
 import About from './Components/About.jsx'
 import Services from './Components/Services.jsx'
 import HireMe from './Components/HireMe.jsx'
-import Work from './Components/work.jsx'
+import Work from './Components/Work_Section.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
